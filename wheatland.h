ef1eefdb0acbfcb71a2351bb73a2d9965070ebec
pouya behzadifar <pouyabeh80@gmail.com>
2021-07-13 00:32:38 +0430

    wheatland and new logos added

---
+++ wheatland.h
@@ -0,0 +1,36 @@
+#ifndef WHEATLAND_H
+#define WHEATLAND_H
+
+#include <QDialog>
+#include <mainwindow.h>
+namespace Ui {
+class wheatland;
+}
+
+class wheatland : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit wheatland(QWidget *parent = nullptr, int _id = 1);
+    ~wheatland();
+
+private slots:
+    void on_kesht_clicked();
+
+    void on_bardasht_clicked();
+
+    void on_upGrade_clicked();
+
+private:
+    Ui::wheatland *ui;
+    int area;
+    bool isKeshting;
+    int keshtAmount;
+    int startDay;
+    int startMonth;
+    int startYear;
+    int id;
+};
+
+#endif // WHEATLAND_H
