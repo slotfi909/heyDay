07de416c6845ed77e5d4081371a22900d232428b
Amir1831 <75390652+Amir1831@users.noreply.github.com>
2021-07-15 17:20:09 +0430

    two functions for handling milk added

--- farm.cpp
+++ farm.cpp
@@ -5,3 +5,53 @@ Farm::Farm(int shenase):owner(shenase)
 
 
 }
+
+
+void Farm::write_milk(int shenase ,int tedad,int day){
+
+struct milk{
+int shenaseP;//male chekasei.
+int day;//roz vared shodan be storage.
+int tedad;//tedadi ke onroz vard storage shode.
+bool spoiled;//kharab shode =>1 , kharab nashode=>0.
+}m;
+m.day=day;
+m.shenaseP=shenase;
+m.tedad=tedad;
+m.spoiled=0;
+ofstream outfile;//for writing in new file.
+outfile.open("milk.txt",ios::app);
+outfile.write((char*)&m,sizeof(m));
+outfile.close();
+}
+
+int Farm::read_milk(int shenase,int day){
+
+struct milk{
+int shenaseP;//male chekasei.
+int day;//roz vared shodan be storage.
+int tedad;//tedadi ke onroz vard storage shode.
+bool spoiled;//kharab shode =>1 , kharab nashode=>0.
+}m;
+
+
+ifstream infile;
+infile.open("milk.txt",ios::in);
+if(infile.is_open()){
+
+infile.seekg(0,ios::end);
+int size=infile.tellg();
+infile.seekg(0,ios::beg);
+while(infile.tellg()<size){
+    infile.read((char*)&m,sizeof(m));
+    if(m.shenaseP==shenase)
+    if(!m.spoiled)
+    if((day-m.day)>=10){
+       m.spoiled=1;
+       return -(m.tedad);//tedadi ke bayad az anbar kam beshe.
+    }
+
+}
+}
+ return 0;
+}
--- farm.h
+++ farm.h
@@ -2,12 +2,16 @@
 #define FARM_H
 #include "person.h"
 #include <QMutex>
+#include <fstream>
+using namespace std;
 class Farm
 {
 public:
     Farm(int);
    person0 owner;
    QMutex mymutex;
+   void write_milk(int shenase ,int tedad,int day);
+   int read_milk(int shenase,int day);
 
 };
 
