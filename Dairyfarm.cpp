46f7be7b67e042ade84722d9af54cd1b5f1dbf3a
Gutsooo <87245405+Gutsooo@users.noreply.github.com>
2021-07-16 10:06:41 +0430

    Update Dairyfarm.cpp

--- Dairyfarm.cpp
+++ Dairyfarm.cpp
@@ -37,7 +37,7 @@ Dairyfarm::Dairyfarm(QWidget *parent, farm* _Myfarm)
 
     setLayout(Layout);
 
-    connect(btn1, SIGNAL(clicked()), this, SLOT(ststus()));
+    connect(btn1, SIGNAL(clicked()), this, SLOT(status()));
     connect(btn2, SIGNAL(clicked()), this, SLOT(feeding()));
     connect(btn3, SIGNAL(clicked()), this, SLOT(removal()));
     connect(btn4, SIGNAL(clicked()), this, SLOT(starting_upgrade()));
