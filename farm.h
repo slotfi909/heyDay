f4dad670e7420e36a33c92af213a4e3054822dfb
Amir1831 <75390652+Amir1831@users.noreply.github.com>
2021-07-16 13:07:17 +0430

    farm update

--- farm.h
+++ farm.h
@@ -1,6 +1,11 @@
 #ifndef FARM_H
 #define FARM_H
 #include "person.h"
+#include "Aviculture_back.h"
+#include "Dairyfarm_back.h"
+#include "Sheepcote_back.h"
+#include "silo.h"
+#include "storage.h"
 #include <QMutex>
 #include <fstream>
 //using namespace std;
@@ -10,6 +15,11 @@ public:
     Farm(int);
    person0 owner;
    QMutex mymutex;
+   Aviculture_back myAvi;
+   Dairyfarm_back myDai;
+   Sheepcote_back myShe;
+   silo mySil;
+   storage mySto;
    void write_milk(int shenase ,int tedad,int day);
    int read_milk(int shenase,int day);
 
